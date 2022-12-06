77dc4d961781ee1fe3c238e55c60e9bd18dc4984
Ahmed Abdelfattah <rererer12@protonmail.com>
2022-12-06 11:33:26 +0200

    edit path of notFound in router.js

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -65,7 +65,7 @@ const LazySignUp = lazy(() => {
 
 const LazyNotFound = lazy(() => {
   return Promise.all([
-    import("./../component/UI/pages/notFound/NotFound"),
+    import("./../component/UI/pages/notfound/NotFound"),
     new Promise((resolve) => setTimeout(resolve, 0)),
   ]).then(([moduleExports]) => {
     document.body.style.overflow = "auto";
